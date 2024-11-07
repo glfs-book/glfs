7614d480e2ee977875a52af7577e3e9442410261
Tox <toxikuu@users.noreply.github.com>
2024-11-07 15:25:18 -0600

    improved switch-theme.sh

--- switch-theme.sh
+++ switch-theme.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-if [ "$1" == "light" ]; then
-  ln -svf light.lfs.css stylesheets/lfs-xsl/lfs.css
-elif [ "$1" == "dark" ]; then
-  ln -svf dark.lfs.css stylesheets/lfs-xsl/lfs.css
-else
-  echo '"'"$1"'" is not "light" or "dark"'
-fi
+case "$1" in
+  light)
+    ln -sfv light.lfs.css stylesheets/lfs-xsl/lfs.css
+    ;;
+  dark)
+    ln -sfv dark.lfs.css stylesheets/lfs-xsl/lfs.css
+    ;;
+  nightdrive)
+    ln -sfv nightdrive.lfs.css stylesheets/lfs-xsl/lfs.css
+    ;;
+  whitepink)
+    ln -sfv whitepink.lfs.css stylesheets/lfs-xsl/lfs.css
+    ;;
+  *)
+    echo 'Invalid theme!' >&2
+    exit 1
+    ;;
+esac
